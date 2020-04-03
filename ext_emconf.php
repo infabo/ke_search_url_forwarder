cdea7cbd0ad5a48be4a0e0fa6f4db1afa4a7dc81
Ingo Fabbri <inf@mmc-agentur.at>
2020-04-03 14:21:31 +0200

    [TASK] raise compatibility to 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.2',
+    'version' => '1.1.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '',
+            'ke_search' => '',
         ),
         'conflicts' => array(),
         'suggests' => array()
